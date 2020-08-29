06a88a1e6e4bf589d2aeaf050e7a95b2a998cadf
b-hayes <bradley.hayes@tithe.ly>
2020-08-29 21:09:59 +1000

    new player controller

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,25 +6,31 @@ extends KinematicBody
 # var b = "text"
 
 export var playerName = "Mr Bean"
-export var health = 100;
-export(Resource) var camera = null;
+export var health = 100
+export var lookSensitivity = 0.1
 
 var velocity = Vector3(0,0,0)
+var hRot = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	$HUD/Name.text = playerName;
 	
 func _process(delta):
+	pass
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		hRot += -event.relative.x
+
+func _physics_process(delta):
 	velocity.z = (Input.get_action_strength("backward") - Input.get_action_strength("forward")) * 5
 	velocity.x = (Input.get_action_strength("strafe right") - Input.get_action_strength("strafe left")) * 5
-	
-	# Move the player	
-	move_and_slide(velocity)
-	
+	rotation_degrees.y = hRot * lookSensitivity
+	print(rotation.y)
+	move_and_slide(velocity.rotated(Vector3(0, 1, 0), rotation.y))
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func attack():
+	
