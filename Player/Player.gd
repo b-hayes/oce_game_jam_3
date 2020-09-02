89898b61a7cea129adef200f4aa8525c67e0fffe
b-hayes <bradley.hayes@tithe.ly>
2020-09-02 22:56:18 +1000

    Testing our move and collide solution instead of move and slide
    the animation speeds and movement speeds all get messed up when changing to this method

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,18 +8,17 @@ extends KinematicBody
 export var playerName = "Mr Bean"
 export var health = 100
 export var lookSensitivity = 0.1
-export var walkingSpeed = 5
+export var walkingSpeed = 1
 
 var velocity = Vector3(0,0,0)
 var hRot = 0
-var gravity : int = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity : int = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	$HUD/Name.text = playerName;
-	$AnimationPlayer.playback_speed  = walkingSpeed
 	
 func _process(delta):
 	pass
@@ -29,21 +28,30 @@ func _input(event):
 		hRot += -event.relative.x
 
 func _physics_process(delta):
-	velocity.z = (Input.get_action_strength("backward") - Input.get_action_strength("forward")) * walkingSpeed
-	velocity.x = (Input.get_action_strength("strafe right") - Input.get_action_strength("strafe left")) * walkingSpeed
+	var direction = Vector3()
+	direction.z = (Input.get_action_strength("backward") - Input.get_action_strength("forward"))
+	velocity.z = direction.z * walkingSpeed
+	direction.x = (Input.get_action_strength("strafe right") - Input.get_action_strength("strafe left"))
+	velocity.x = direction.x * walkingSpeed
 	rotation_degrees.y = hRot * lookSensitivity
 	#Apply gravity
 	velocity.y -= delta * gravity
-	move_and_slide(velocity.rotated(Vector3(0, 1, 0), rotation.y))
+	#velocity = move_and_slide(velocity.rotated(Vector3(0, 1, 0), rotation.y))
+	
+	#TESTing move and cilide instead
+	direction.y = - delta * gravity
+	var collisions = move_and_collide(direction.rotated(Vector3(0, 1, 0), rotation.y))
 	
 	#Walking Animation
-	if velocity.z || velocity.x:
+	$AnimationPlayer.playback_speed = max(abs(velocity.z), abs(velocity.x)) * 10
+	if direction.z || direction.x:
 		if	$AnimationPlayer.current_animation != "Walking":
 			$AnimationPlayer.play("Walking")
 	elif $AnimationPlayer.current_animation == "Walking":
 		$AnimationPlayer.stop()
+		
 	#Turn torso so strafe left and right doesnt look odd with walking animation
-	var torsoRotation = (Input.get_action_strength("strafe right") - Input.get_action_strength("strafe left")) * 45
+	var torsoRotation = direction.x * 45
 	$Body/Torso.rotation_degrees.z = torsoRotation
 		
 func attack():
