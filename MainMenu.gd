e21636b9a90ee767ebbe3c5ca5fa3c94f8f411f4
b-hayes <bradley.hayes@tithe.ly>
2020-09-07 20:32:20 +1000

    menu is now a scne that can controll its visibility with escape

--- MainMenu.gd
+++ MainMenu.gd
@@ -4,16 +4,27 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var menu
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	menu = $"Menu"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		#get_tree().quit()
+		if menu.is_inside_tree():
+			remove_child(menu)
+		else:
+			add_child(menu)
+		
+
+	if event.is_action_pressed("Toggle Fullscreen"):
+		OS.window_fullscreen = !OS.window_fullscreen
 
 
 func _on_PLAY_pressed():
